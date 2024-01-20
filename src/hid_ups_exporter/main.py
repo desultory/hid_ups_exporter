cdd0a9efb83cba7daa9e60f61d3a43f02a654f04
Zen <z@pyl.onl>
2024-01-20 13:22:19 -0600

    simplfy main
    Signed-off-by: Zen <z@pyl.onl>

--- src/hid_ups_exporter/main.py
+++ src/hid_ups_exporter/main.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
 
 from hid_ups_exporter import HIDUPSExporter
-from zenlib.util import init_logger, init_argparser, process_args
+from prometheus_exporter import DEFAULT_EXPORTER_ARGS
+from zenlib.util import get_kwargs
 
 
 def main():
-    argparser = init_argparser(prog=__package__, description='HID based UPS exporter for Prometheus.')
-    logger = init_logger(__package__)
+    arguments = DEFAULT_EXPORTER_ARGS.copy()
+    arguments += [{'flags': ['--run-forever'], 'help': 'Run forever, even if failing.', 'action': 'store_true'},
+                  {'flags': ['--max-fails'], 'help': 'Maximum number of fails before killing the listener.', 'type': int}]
 
-    argparser.add_argument('-p', '--port', type=int, nargs='?', help='Port to listen on.')
-    argparser.add_argument('-a', '--address', type=str, nargs='?', help='Address to listen on.')
-    argparser.add_argument('--run-forever', action='store_true', help='Run forever, even if failing.')
-    argparser.add_argument('--max-fails', type=int, nargs='?', help='Maximum number of fails before killing the listener.')
-    args = process_args(argparser, logger=logger)
-
-    kwargs = {'logger': logger, 'run_forever': args.run_forever}
-
-    if args.port:
-        kwargs['listen_port'] = args.port
-    if args.address:
-        kwargs['listen_ip'] = args.address
-    if args.max_fails:
-        kwargs['max_fails'] = args.max_fails
+    kwargs = get_kwargs(package=__package__, description='HID UPS Prometheus Exporter', arguments=arguments)
 
     exporter = HIDUPSExporter(**kwargs)
     exporter.start()
